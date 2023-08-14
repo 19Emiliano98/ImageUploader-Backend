b8761853996808893b2265697c1af7a7ff86706f
Emiliano Caballero <emiliano.caballero98@gmail.com>
2023-08-14 12:15:29 -0300

    reconfigure ts en server

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,11 +1,13 @@
 import express from 'express';
 
-import { principal } from './routes/principal.ts';
-
 const app = express();
+app.use(express.json());
 const PORT:number = 8080;
 
-app.use( '/principal', principal );
+app.get('/ping', (_req, res) =>{
+  console.log('someone pinged here!!!');
+  res.send('pong');
+})
 
 app.listen( PORT, () => {
   console.log( `Example app listening on port: ${PORT}. Login in: https://localhost:${PORT}/principal` );
