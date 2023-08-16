a8ee9214fef9c62b35c2d87514f692ffbc398f84
Emiliano Caballero <emiliano.caballero98@gmail.com>
2023-08-16 13:47:52 -0300

    Funciona la carga de imagen de front

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,51 +1,13 @@
-import { useState } from 'react';
+import React from 'react';
+import ImageUploadForm from './components/ImageUploadForm';
 
-const API:string = 'http://localhost:8080/upload';
-
-function App() {
-  const [selectedImage, setSelectedImage] = useState(null);
-  const [nameImage, setNameImage] = useState('');
-
-  const handleImageChange = (event:any) => {
-    const imageFile = event.target.files[0];
-    console.log(event.target.files[0]);
-    
-    setSelectedImage(imageFile);
-    setNameImage(imageFile.name)
-  };
-
-  const handleUpload = (e:any) => {
-    e.preventDefault();
-    if (selectedImage) {
-      const formData = new FormData();
-      formData.append('image', selectedImage, nameImage);
-      
-      //? Aquí puedes realizar la llamada a la API para subir la imagen
-      //? usando fetch u otra librería de manejo de peticiones HTTP.
-      //? Por ejemplo:
-
-      fetch( API, {
-        method: 'POST',
-        body: formData.get('image')
-      })
-      .then(response => response.json())
-      .then(data => {
-        console.log('Imagen subida con éxito', data)
-        console.log(formData.get('image'));
-        
-      })
-      .catch(error => {
-        console.error('Error al subir la imagen', error)
-      })
-    }
-  };
-
+const App: React.FC = () => {
   return (
-    <form method='POST' action={`${API}`} onSubmit={handleUpload} encType="multipart/form-data">
-      <input type="file" name='image' accept="image/*" onChange={handleImageChange} />
-      <button type='submit'>Subir imagen</button>
-    </form>
+    <div>
+      <h1>Image Upload App</h1>
+      <ImageUploadForm />
+    </div>
   );
-}
+};
 
 export default App;
---
+++ client/src/components/ImageUploadForm.tsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+
+const ImageUploadForm: React.FC = () => {
+  const [selectedImage, setSelectedImage] = useState<File | null>(null);
+
+  const handleImageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files && e.target.files.length > 0) {
+      setSelectedImage(e.target.files[0]);
+    }
+  };
+
+  const handleUpload = async () => {
+    if (selectedImage) {
+      const formData = new FormData();
+      formData.append('image', selectedImage);
+
+      try {
+        const response = await fetch('http://localhost:8080/upload', {
+          method: 'POST',
+          body: formData,
+        });
+
+        if (response.ok) {
+          console.log('Image uploaded successfully');
+        } else {
+          console.error('Failed to upload image');
+        }
+      } catch (error) {
+        console.error('Error uploading image:', error);
+      }
+    }
+  };
+
+  return (
+    <div>
+      <input type="file" accept="image/*" onChange={handleImageChange} />
+      <button onClick={handleUpload}>Upload Image</button>
+    </div>
+  );
+};
+
+export default ImageUploadForm;
